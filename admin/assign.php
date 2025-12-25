5c0f65cb9694e29ec58dab8436820fddd1f3dca2
RafiBakhtiar23 <rafibakkhtiar@gmail.com>
2025-12-25 17:37:48 +0800

    Update assign.php

--- admin/assign.php
+++ admin/assign.php
@@ -12,6 +12,7 @@ $teknisi = mysqli_query($conn,"
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Assign Teknisi</title>
 <link rel="stylesheet" href="../assets/css/style.css">
 </head>
