0f7f697be5033a66ec2f92c9de9b47c4430bc485
RafiBakhtiar23 <rafibakkhtiar@gmail.com>
2025-12-25 17:36:29 +0800

    Add viewport meta tag to dashboard.php

--- siswa/dashboard.php
+++ siswa/dashboard.php
@@ -15,6 +15,7 @@ $data = mysqli_query($conn, "
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Dashboard Siswa</title>
     <link rel="stylesheet" href="../assets/css/style.css">
 </head> 
