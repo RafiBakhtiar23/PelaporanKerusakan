b7100bf416eea01500e14096f724433d08fc8b11
RafiBakhtiar23 <rafibakkhtiar@gmail.com>
2025-12-25 17:38:16 +0800

    Update detail.php

--- admin/detail.php
+++ admin/detail.php
@@ -18,6 +18,7 @@ $r = mysqli_fetch_assoc($q);
 <!DOCTYPE html>
 <html>
 <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Detail Laporan</title>
 <link rel="stylesheet" href="../assets/css/style.css">
 </head>
